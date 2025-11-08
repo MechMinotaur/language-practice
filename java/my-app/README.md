333f633691985848cb761d88d99bbe608a67690f
Tim Burch <tim.burch1@outlook.com>
2025-11-08 13:32:27 -0600

    Add sqlite-jdbc to my-app.

--- java/my-app/README.md
+++ java/my-app/README.md
@@ -1,9 +1,14 @@
 # my-app
 
-Simple app to demonstrate knowledge of the Java programming language. I followed the guide [here](https://maven.apache.org/guides/getting-started/maven-in-five-minutes.html) to get a very basic Maven project up and running.
+Simple app to demonstrate knowledge of the Java programming language.
+I followed the following guides:
+
+
+- Get a basic [Maven](https://maven.apache.org/guides/getting-started/maven-in-five-minutes.html) project up and running.
+- Setup a SQLite database following [these instructions](https://github.com/xerial/sqlite-jdbc). The Download section was essential for Maven.
 
 # Setup and Execution
 1. Install the latest JDK.
 1. Install Maven.
-1. Execute build.sh
-1. Execute run.sh
+1. Run `./build.sh`
+1. Run `./run.sh`
--- java/my-app/src/main/java/com/mycompany/app/App.java
+++ java/my-app/src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,11 @@
 package com.mycompany.app;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import com.mycompany.app.controller.UserController;
 import com.mycompany.app.model.UserModel;
 import com.mycompany.app.view.UserView;
@@ -8,7 +14,40 @@ import com.mycompany.app.view.UserView;
  * Hello world!
  */
 public class App {
+    public static void CreateDatabase(){
+        // NOTE: Connection and Statement are AutoCloseable.
+        //       Don't forget to close them both in order to avoid leaks.
+        try
+        (
+          // create a database connection
+          Connection connection = DriverManager.getConnection("jdbc:sqlite:sample.db");
+          Statement statement = connection.createStatement();
+        )
+        {
+          statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+          statement.executeUpdate("drop table if exists person");
+          statement.executeUpdate("create table person (id integer, name string)");
+          statement.executeUpdate("insert into person values(1, 'leo')");
+          statement.executeUpdate("insert into person values(2, 'yui')");
+          ResultSet rs = statement.executeQuery("select * from person");
+          while(rs.next())
+          {
+            // read the result set
+            System.out.println("name = " + rs.getString("name"));
+            System.out.println("id = " + rs.getInt("id"));
+          }
+        }
+        catch(SQLException e)
+        {
+          // if the error message is "out of memory",
+          // it probably means no database file is found
+          e.printStackTrace(System.err);
+        }
+    }
+
     public static void main(String[] args) {
+        CreateDatabase();
         var userModel = new UserModel();
         var userView = new UserView();
         var UserController = new UserController();
