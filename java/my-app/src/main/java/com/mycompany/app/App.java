39d8e4e4a8add8d673390d5d615353cb4aaec4b5
Tim Burch <tim.burch1@outlook.com>
2025-11-08 12:03:14 -0600

    Create a separate file and import.

--- java/my-app/src/main/java/com/mycompany/app/App.java
+++ java/my-app/src/main/java/com/mycompany/app/App.java
@@ -1,10 +1,13 @@
 package com.mycompany.app;
 
+import com.mycompany.app.model.UserModel;
+
 /**
  * Hello world!
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        var userModel = new UserModel();
+        System.out.println("Hello from App!");
     }
 }
---
+++ java/my-app/src/main/java/com/mycompany/app/model/UserModel.java
@@ -0,0 +1,7 @@
+package com.mycompany.app.model;
+
+public class UserModel {
+    public UserModel(){
+        System.out.println("Hello from UserModel!");
+    }
+}
