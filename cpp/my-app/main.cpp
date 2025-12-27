1fc236f672e6853f84d086cd074a36da8b0b14c4
Tim Burch <tim.burch1@outlook.com>
2025-12-26 23:59:23 -0600

    Setup CPM for package management.

--- cpp/my-app/main.cpp
+++ cpp/my-app/main.cpp
@@ -1,14 +1,22 @@
+#include <fstream>
 #include <iostream>
+#include <nlohmann/json.hpp>
 #include <string>
 #include <vector>
 
 int main() {
-  std::vector<std::string> msg{"Hello", "C++",      "World",
+  std::string exampleJsonFP =
+      std::filesystem::path(__FILE__).parent_path() / "example.json";
+  std::ifstream f(exampleJsonFP);
+  nlohmann::json data = nlohmann::json::parse(f);
+
+  std::vector<std::string> msg{"Hello", "C++",      data["Hello"],
                                "from",  "Code-OSS", "and Clangd!"};
 
   for (const std::string &word : msg) {
     std::cout << word << " ";
   }
   std::cout << std::endl;
+
   return 0;
 }
